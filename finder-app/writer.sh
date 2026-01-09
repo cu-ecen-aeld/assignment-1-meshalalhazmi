90208e02c9e42fddb8f97a9ea8f15ff280950254
Meshal <meshal.alhazmi@gmail.com>
2026-01-09 12:04:46 -0600

    wirte file script

---
+++ finder-app/writer.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#Accepts the following arguments: the first argument is a full path to a file (including filename) on the filesystem, referred to below as writefile; the second argument is a text string which will be written within this file, referred to below as writestr
+#Exits with value 1 error and print statements if any of the arguments above were not specified
+if [ $# -lt 2 ]
+then
+    echo "Usage: $0 <writefile> <writestr>"
+    exit 1
+fi
+
+writefile=$1
+writestr=$2
+#Creates a new file with name and path writefile with content writestr, overwriting any existing file and creating the path if it doesn’t exist. Exits with value 1 and error print statement if the file could not be created
+mkdir -p "$(dirname "$writefile")"
+echo "$writestr" > "$writefile" 
+if [ $? -ne 0 ]
+then
+    echo "Error: Could not create file $writefile"
+    exit 1
+fi
